587000fc283e1a4f280d2300e8ce7a0226b6bc53
llindemann <leonie.lindemann.ll@gmail.com>
2024-03-29 20:37:57 +0100

    feat: random fruit sprites

--- scenes/fruit/fruit_controller.gd
+++ scenes/fruit/fruit_controller.gd
@@ -1,8 +1,28 @@
+@tool
 extends Area2D
+class_name Fruit
 
 signal collected
 
+# sprite frames for different fruit types
+@export var sprites : Array[SpriteFrames]
+
 var active = true
+
+# Called when the node enters the scene tree for the first time.
+# randomize fruits sprit type
+func _ready():
+	randomize()
+	$AnimatedSprite2D.sprite_frames = sprites[randi_range(0, len(sprites)-1)]
+	$AnimatedSprite2D.play()
+
+# Reset fruit object to default active state
+func reset():
+	active = true
+	show()
+	$CollisionShape2D.disabled = false
+
+
 
 func _on_body_entered(body):
 	if active and body.get_name() == "player":
@@ -12,8 +32,3 @@ func _on_body_entered(body):
 		$CollisionShape2D.set_deferred("disabled", true)
 		
 		emit_signal("collected")
-
-func reset():
-	active = true
-	show()
-	$CollisionShape2D.set_deferred("disabled", false)
